7dfa0f36970bb077fa044db2cc8279406eea544d
shinybaubles <Shinibaubles@gmail.com>
2024-12-16 19:12:37 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@ export default {
         {
           "slug": "003",
           "name": "Long, Lonesome Road",
-          "status": "start"
+          "status": "success"
         },
       ],
       "pilots": [
