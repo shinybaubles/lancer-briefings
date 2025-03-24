fbab6f48479e0d0e0b759720eb7378878eb9e239
shinybaubles <Shinibaubles@gmail.com>
2025-03-23 23:43:01 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -160,6 +160,14 @@ export default {
           "frame": "Emperor",
           "mech": "World-Is-Mine"
         },
+        {
+          "callsign": "Casper",
+          "alias": 'Samara al-Rashid',
+          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "corpro": "GMS",
+          "frame": "Chomolungma",
+          "mech": "World-Is-Mine"
+        },
       ],
       "header": {
         "planet": "Evil Wizard's Lair?",
